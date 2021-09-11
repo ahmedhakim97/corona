514ad6f366427f04e0e840456a2ca19c64a03e21
ahmedhakim97 <hakeem.shr@gmail.com>
2021-09-12 01:16:45 +0200

    add header , navbar and smoothly scroll button

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
-let x= Math.round(Math.random()*255)
-
-let y = Math.round(Math.random()*255)
-
-let z = Math.round(Math.random()*255)
+// for navbar scroll ; 
+$(window).scroll(function(){
 
 
-$(".test").css("backgroundColor" , `rgb(${x},${y}, ${z})`);
+    let wScroll =   $(window).scrollTop();
+    let headerOffset = $("#banner").offset().top;
+ 
+    if(wScroll > 100)
+    {
+    
+     $("nav ul li a , nav #logo ").css("color" ,"black");
+        $("nav").addClass("fixed-top");
+        $("#btnScroll").fadeIn(2000)
+    }
+    else 
+    {
+     $("nav").removeClass("fixed-top");
+     $("#btnScroll").fadeOut(2000)
+ 
+    }
+ 
+ });
 
-
-$(".test").click(function(){
-var currentcolor = $(this).css("backgroundColor")
-$("p").css("color" , currentcolor);
-
-})
+ $("#btnScroll").click(function(){
+    $(this).css("border" ,"1px solid  rgb(43,86,245)")
+$("html , body").animate({scrollTop : `0px`} , 1000)
+});
